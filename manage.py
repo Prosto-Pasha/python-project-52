302c05b7d7c0e6d07f6337a5efd16cbd090fa712
Pavel Belyavskiy <prostopasha@gmail.com>
2023-05-05 15:36:02 +0300

    Update manage.py for railway

--- manage.py
+++ manage.py
@@ -2,6 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+
+from django.core.management.commands.runserver import Command as runserver
+
+
+# Получаем переменные окружения
+load_dotenv()  # PORT
+PORT = os.getenv('PORT')
 
 
 def main():
@@ -15,6 +23,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    runserver.default_port = PORT
     execute_from_command_line(sys.argv)
 
 
