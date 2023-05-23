6799cef98ef80db176f27fe0609bbf0a7312fe64
Pavel Belyavskiy <prostopasha@gmail.com>
2023-05-23 21:25:37 +0300

    Refactor users

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,5 +1,8 @@
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class SignUpForm(UserCreationForm):
@@ -16,11 +19,19 @@ class SignUpForm(UserCreationForm):
 
 
 class ChangeForm(UserChangeForm):
+    password1 = forms.CharField(label=_("Password"),
+                                min_length=3,
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Confirm password"),
+                                min_length=3,
+                                widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = (
             'first_name',
             'last_name',
-            'username'
+            'username',
+            'password1',
+            'password2'
         )
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,96 +2,72 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from task_manager.users.forms import SignUpForm, ChangeForm
-
-from django.http import HttpResponse
-
-
-class IndexView(View):
-
-    def get(self, request, *args, **kwargs):
-        users = User.objects.all()
-        return render(request, 'users/index.html', context={
-            'users': users,
-        })
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 
-class UserCreateView(View):
+class IndexView(ListView):
+    model = User  # Model name
+    context_object_name = 'users'  # Template list name
+    template_name = 'users/index.html'  # Template path
 
-    def get(self, request, *args, **kwargs):
-        form = SignUpForm()
-        return render(request, 'users/create.html', {'form': form})
 
-    def post(self, request, *args, **kwargs):
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = request.POST["username"]
-            password = request.POST["password1"]
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-            messages.success(request, _('Account created successfully'))
-            return redirect('users_index')
-        return render(request, "users/create.html", {'form': form})
+class UserCreateView(CreateView):
+    model = User
+    form_class = SignUpForm
+    template_name = 'users/create.html'  # Template path
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        # form.instance.user = self.request.user
+        messages.success(self.request, _('Account created successfully'))
+        return super(UserCreateView, self).form_valid(form)
 
 
 class UserUpdateView(View):
 
-    def get(self, request, *args, **kwargs):
-        request_id = request.user.id
+    def check_user(self, request, param_id):
         if not request.user.is_authenticated:
             messages.error(request, _(
                 'Вы не авторизованы! Пожалуйста, выполните вход.'
             ))
             return redirect('login')
-        elif kwargs.get('id') != request_id:
+        elif param_id != request.user.id:
             messages.error(request, _(
                 'У вас нет прав для изменения другого пользователя.'
             ))
             return redirect('users_index')
 
-            #return HttpResponse("User ID %s." % kwargs.get('id'))
-            #return HttpResponse("User ID %s." % request.user.id)
+    def get(self, request, *args, **kwargs):
+        param_id = kwargs.get('id')
+        self.check_user(request, param_id)
         form = SignUpForm(instance=request.user)
         return render(
             request,
             'users/update.html',
-            {'form': form, 'user_id': request_id}
+            {'form': form, 'user_id': request.user.id}
         )
 
     def post(self, request, *args, **kwargs):
-        request_id = request.user.id
-        if not request.user.is_authenticated:
-            messages.error(request, _(
-                'Вы не авторизованы! Пожалуйста, выполните вход.'
-            ))
-            return redirect('login')
-        elif kwargs.get('id') != request_id:
-            messages.error(request, _(
-                'У вас нет прав для изменения другого пользователя.'
-            ))
-            return redirect('users_index')
-        current_user = User.objects.get(id=request_id)
+        param_id = kwargs.get('id')
+        self.check_user(request, param_id)
+        current_user = User.objects.get(id=request.user.id)
         form = ChangeForm(request.POST, instance=current_user)
         if form.is_valid():
             changed_user = form.save(commit=False)
             changed_user.user = request.user
-
-            print(1, form.cleaned_data['password'])
-
-            # changed_user.password = form.cleaned_data['password1']
             changed_user.save()
             login(request, current_user)
             messages.success(request, _('Пользователь успешно изменён'))
-            return redirect(to='users_index')
+            return redirect('users_index')
         return render(
             request,
             'users/update.html',
-            {'form': form, 'user_id': request_id}
+            {'form': form, 'user_id': request.user.id}
         )
 
 
